94d3c294ba36b36ca7d923de09735f4238b3b10e
Mike. <miguelmoraisaraujo2011@gmail.com>
2025-05-17 15:15:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 console.log("test")
+
 function att() {
     const agora = new Date();
-    const horas = String(agora.getHours()).padStart(2, 0)
-    const minutos = String(agora.getMinutes()).padStart(2, 0)
-    const segundos = String(agora.getSeconds()).padStart(2, 0)
-    const relogio = horas + ":" + minutos + ":" + segundos
-    document.getElementById("relogio").textContent = relogio
+    const horas = String(agora.getHours()).padStart(2, '0');
+    const minutos = String(agora.getMinutes()).padStart(2, '0');
+    const segundos = String(agora.getSeconds()).padStart(2, '0');
+    const relogio = `${horas}:${minutos}:${segundos}`;
+    document.getElementById("relogio").textContent = relogio;
 }
-setInterval(att, 1000)
+setInterval(att, 1000);
+
 
 function iniciarContagem() {
     const agora = new Date();
-    const dataSelecionada = document.getElementById("dataSelecionada").value 
-    const datafinal = new Date(dataSelecionada)
-    const diferenca = datafinal - agora
+    const dataSelecionada = document.getElementById("dataSelecionada").value;
+    const datafinal = new Date(dataSelecionada);
+    const diferenca = datafinal - agora;
 
-    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24))
-    const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) /1000 * 60 * 60)
-    const minutos = Math.floor((diferenca % (1000 * 60 * 60 )) / 1000 * 60)
-    const segundos = Math.floor((diferenca % (1000 * 60 ) ) / 1000)
+    if (diferenca <= 0) {
+        document.getElementById('Temporizador').textContent = "Tempo Expirado!";
+        return;  
+    }
 
-    console.log(agora)
-    console.log(dataSelecionada)
-    console.log(datafinal)
-    console.log(diferenca)
-    console.log(dias)
-    console.log(horas)
-    console.log(minutos)
-    console.log(segundos)
-}   
+    let dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    let horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+    let segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+    
+    horas = String(horas).padStart(2, '0');
+    minutos = String(minutos).padStart(2, '0');
+    segundos = String(segundos).padStart(2, '0');
+
+
+    document.getElementById('Temporizador').textContent = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+}
+
+setInterval(iniciarContagem, 1000);
